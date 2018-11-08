111e7cc01421f816c4a1dfa200e8ae743986731a
franciscoh017 <franciscoh017@gmail.com>
2018-11-07 20:28:09 -0400

    comite

---
+++ ptd/js/accordion.js
@@ -0,0 +1,22 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    this.classList.toggle("is-accordion");
+    if(this.children[0].getAttribute("data-icon")=="chevron-down")
+    {
+        this.children[0].setAttribute("data-icon", "chevron-right");
+    }
+    else{
+        this.children[0].setAttribute("data-icon", "chevron-down");
+    }
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
